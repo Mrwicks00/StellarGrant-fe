1ffb6319c8bcb5b8e4990383747a19b4dc4fc1e2
Maxwell316 <blackmanowen05@gmail.com>
2026-04-26 18:09:13 +0100

    feat: implement detailed error mapping for Soroban errors

--- client/src/errors/StellarGrantsError.ts
+++ client/src/errors/StellarGrantsError.ts
@@ -16,3 +16,24 @@ export class SorobanRevertError extends StellarGrantsError {
     this.name = "SorobanRevertError";
   }
 }
+
+export class UnauthorizedError extends SorobanRevertError {
+  constructor(details?: unknown) {
+    super("Unauthorized: You do not have permission to perform this action.", details);
+    this.name = "UnauthorizedError";
+  }
+}
+
+export class GrantNotFoundError extends SorobanRevertError {
+  constructor(details?: unknown) {
+    super("Grant not found: The requested grant does not exist or has been removed.", details);
+    this.name = "GrantNotFoundError";
+  }
+}
+
+export class InvalidStateError extends SorobanRevertError {
+  constructor(message: string, details?: unknown) {
+    super(message, details);
+    this.name = "InvalidStateError";
+  }
+}
