89eb087d62791e743802c33f2aa1029031767650
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2026-06-25 13:41:27 +0100

    chore: update delegate placeholder

--- stellargrant-contracts/contracts/stellar-grants/src/delegate.rs
+++ stellargrant-contracts/contracts/stellar-grants/src/delegate.rs
@@ -1,7 +1,4 @@
 use soroban_sdk::{Address, Env};
 
-use crate::types::ContractError;
-
-pub fn placeholder(_env: &Env, _address: &Address) -> Result<(), ContractError> {
-    Ok(())
+pub fn placeholder(_env: &Env, _address: &Address) {
 }
